bf8c0bf9d91daa1845e5a307a18ce7dfcf055949
David Eilers <davidwesseleilers@hotmail.com>
2017-06-08 15:19:02 +0200

    Simple Rest Implementation

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/groups')
+@app.route('/groups', methods=['GET', 'POST'] )
 def index():
-    return "dit is groep"
+    if request.method == 'GET':
+        return 'This is /groups'
+    else:
+        return 'This is /groups POST'
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=True)
