ed65a0907f257e46b2e0c5b93febaf7ff31c3991
David Eilers <davidwesseleilers@hotmail.com>
2017-06-14 12:43:12 +0200

    Added Extra Routes

--- server.py
+++ server.py
@@ -9,6 +9,17 @@ def index():
         return 'This is /groups'
     else:
         return 'This is /groups POST'
+@app.route('/users/invite/<id>', methods=['POST'])
+def send_invite(id):
+    return 'Invite send to id: %s' % id
+
+@app.route('/users/invite/accept/<group_id>', methods=['POST'])
+def accept_invite(group_id):
+    return 'Invite accepted from group_id: %s' % group_id
+
+@app.route('/groups/<group_id>/user/<id>', methods=['DELETE'])
+def remove_user_from_group(group_id, id):
+    return 'User_id {} removed from group_id {}'.format(group_id, id)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
