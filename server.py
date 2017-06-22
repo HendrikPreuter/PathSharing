e388e7ce843596935f9011e7a6af8141c07590ec
David Eilers <davidwesseleilers@hotmail.com>
2017-06-22 13:53:06 +0200

    Changed route responses to json

--- server.py
+++ server.py
@@ -1,58 +1,55 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 # TODO: figure out how CORS works exactly, possibly remove CORS(app) and add @cross_origin() where needed.
 CORS(app)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return 'This is the homepage GET'
+        return jsonify({'message' : 'This is the homepage GET'})
     else:
-        return 'This is the homepage POST'
+        return jsonify({'message': 'This is the homepage POST'})
 
 
 @app.route('/groups', methods=['GET', 'POST'])
 def groups():
     if request.method == 'GET':
-        return 'This is /groups GET'
+        return jsonify({'message' : 'This is /groups GET'})
     else:
-        return 'This is /groups POST'
+        return jsonify({'message' : 'This is /groups POST'})
 
 
 @app.route('/user', methods=['GET', 'POST'])
 def user():
     if request.method == 'GET':
-        return 'This is /user GET'
+        return jsonify({'message' : 'This is /user GET'})
     else:
-        return 'This is /user POST'
-
+        return jsonify({'message' : 'This is /user POST'})
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
     if request.method == 'GET':
-        return 'This is /about GET'
+        return jsonify({'message' : 'This is /about GET'})
     else:
-        return 'This is /about POST'
-
+        return jsonify({'message' : 'This is /about POST'})
 
 @app.route('/users/invite/<id>', methods=['POST'])
 def send_invite(id):
-    return 'Invite sent to id: %s' % id
-
+    msg = 'Invite sent to id: ' + id
+    return jsonify({'message' : msg})
 
 @app.route('/users/invite/accept/<group_id>', methods=['POST'])
 def accept_invite(group_id):
-    return 'Invite accepted from group_id: %s' % group_id
+    msg = 'Invite accepted from group_id: ' + group_id
+    return jsonify({'message' : msg})
 
 
 @app.route('/groups/<group_id>/user/<id>', methods=['DELETE'])
 def remove_user_from_group(group_id, id):
-    return 'User_id {} removed from group_id {}'.format(group_id, id)
-
+    msg = 'User_id ' + id + ' removed from group_id ' + group_id
+    return jsonify({'message' : msg})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
